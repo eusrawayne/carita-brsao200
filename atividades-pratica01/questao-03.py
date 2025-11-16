84a8683c1c333034510553af0608dff974041cad
Carita Fonseca <eusrawayne@gmail.com>
2025-11-15 23:49:24 -0300

    descrição das alterações

---
+++ atividades-pratica01/questao-03.py
@@ -0,0 +1,16 @@
+"""
+3- Calculadora de Volume
+* Crie um programa que calcula o volume de uma caixa retangular. 
+Use as seguintes dimensões:
+
+* Comprimento: 12 cm
+* Largura: 14 cm
+* Altura: 20 cm
+O programa deve calcular o volume e exibir o resultado em cm³.
+"""
+
+comprimento = 12  # em cm
+largura = 14      # em cm
+altura = 20       # em cm
+volume = comprimento * largura * altura
+print("O volume da caixa retangular é:", volume, "cm³")
